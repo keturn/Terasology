e95fc8d10da615710aecb49a68b8552eebeba3b8
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-05-01 17:22:19 -0700

    WIP: test: ModuleManager classpath predicate tests

--- engine/src/main/java/org/terasology/engine/core/module/ModuleManager.java
+++ engine/src/main/java/org/terasology/engine/core/module/ModuleManager.java
@@ -24,6 +24,7 @@ import org.terasology.gestalt.module.TableModuleRegistry;
 import org.terasology.gestalt.module.dependencyresolution.DependencyInfo;
 import org.terasology.gestalt.module.dependencyresolution.DependencyResolver;
 import org.terasology.gestalt.module.dependencyresolution.ResolutionResult;
+import org.terasology.gestalt.module.sandbox.API;
 import org.terasology.gestalt.module.sandbox.APIScanner;
 import org.terasology.gestalt.module.sandbox.ModuleSecurityManager;
 import org.terasology.gestalt.module.sandbox.ModuleSecurityPolicy;
@@ -36,14 +37,19 @@ import org.terasology.gestalt.naming.Name;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.ReflectPermission;
+import java.net.JarURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.security.Policy;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class ModuleManager {
@@ -62,15 +68,34 @@ public class ModuleManager {
     private final ModuleInstallManager installManager;
     private final Module engineModule;
 
+    Supplier<PathManager> pathManagerFactory = PathManager::getInstance;
+
     public ModuleManager(String masterServerAddress) {
         this(masterServerAddress, Collections.emptyList());
     }
 
     public ModuleManager(String masterServerAddress, List<Class<?>> classesOnClasspathsToAddToEngine) {
-        PathManager pathManager = PathManager.getInstance();  // get early so if it needs to initialize, it does it now
+        PathManager pathManager = pathManagerFactory.get();  // get early so if it needs to initialize, it does it now
 
         metadataReader = newMetadataReader();
 
+        moduleFactory = newModuleFactory(metadataReader);
+
+        registry = new TableModuleRegistry();
+
+        engineModule = createAndRegisterEngineModule(classesOnClasspathsToAddToEngine);
+
+        loadModulesFromApplicationPath(pathManager);
+
+        ensureModulesDependOnEngine();
+
+        setupSandbox();
+        loadEnvironment(Sets.newHashSet(engineModule), true);
+        installManager = new ModuleInstallManager(this, masterServerAddress);
+    }
+
+    static ModuleFactory newModuleFactory(ModuleMetadataJsonAdapter metadataReader) {
+        final ModuleFactory moduleFactory;
         if (Boolean.getBoolean(LOAD_CLASSPATH_MODULES_PROPERTY)) {
             moduleFactory = new ClasspathCompromisingModuleFactory();
         } else {
@@ -82,18 +107,7 @@ public class ModuleManager {
         mmlm.put(TerasologyConstants.MODULE_INFO_FILENAME.toString(), metadataReader);
         mmlm.put("engine-module.txt", metadataReader);  // FIXME: this should be *only* for engine-module.
         // FIXME: …or maybe engine-module doesn't need a special filename anymore?
-
-        registry = new TableModuleRegistry();
-
-        engineModule = loadEngineModule(classesOnClasspathsToAddToEngine);
-
-        loadModulesFromApplicationPath(pathManager);
-
-        ensureModulesDependOnEngine();
-
-        setupSandbox();
-        loadEnvironment(Sets.newHashSet(engineModule), true);
-        installManager = new ModuleInstallManager(this, masterServerAddress);
+        return moduleFactory;
     }
 
     /**
@@ -118,40 +132,77 @@ public class ModuleManager {
     private void loadModulesFromApplicationPath(PathManager pathManager) {
         ModulePathScanner scanner = new ModulePathScanner(moduleFactory);
         List<File> paths = pathManager.getModulePaths().stream().map(Path::toFile).collect(Collectors.toList());
-        scanner.scan(registry, paths);
+         scanner.scan(registry, paths);
     }
 
-    private Module loadEngineModule(List<Class<?>> classesOnClasspathsToAddToEngine) {
-        Module packageModule = moduleFactory.createPackageModule("org.terasology.engine");
+    private Module createAndRegisterEngineModule(List<Class<?>> classesOnClasspathsToAddToEngine) {
+        Module engine = loadAndConfigureEngineModule(moduleFactory, classesOnClasspathsToAddToEngine);
+        registry.add(engine);
+        return engine;
+    }
 
+    /**
+     * Load and configure the engine module.
+     * <p>
+     * The engine module is the parts of the engine which are available to be called directly
+     * from other modules. Unlike other modules, engine classes are on the classpath and not
+     * restricted by the ModuleClassLoader.
+     * <p>
+     * This function is static so it can be tested without needing a ModuleManager instance.
+     *
+     * @param moduleFactory used to create the module
+     * @param classesOnClasspathsToAddToEngine added to the module's reflections manifest
+     */
+    static Module loadAndConfigureEngineModule(ModuleFactory moduleFactory, List<Class<?>> classesOnClasspathsToAddToEngine) {
+        Module packageModule = moduleFactory.createPackageModule("org.terasology.engine");
         // We need to add reflections from our subsystems and other classes.
         ConfigurationBuilder config = new ConfigurationBuilder();
         Reflections packageReflections = packageModule.getModuleManifest();
         Set<Scanner> scanners = packageReflections.getConfiguration().getScanners();
         config.setScanners(scanners.toArray(new Scanner[0]));
 
+        Collection<File> classPaths = new HashSet<>(packageModule.getClasspaths());
         for (Class<?> aClass : classesOnClasspathsToAddToEngine) {
-            config.addUrls(ClasspathHelper.forClass(aClass));
+            URL url = ClasspathHelper.forClass(aClass);
+            config.addUrls(url);
+            classPaths.add(urlToFile(url));
         }
 
-        // TODO: is this using reflections.cache?
-        Reflections reflectionsWithSubsystems = new Reflections(config);
-        packageReflections.merge(reflectionsWithSubsystems);
+        if (!config.getUrls().isEmpty()) {
+            Reflections reflectionsWithSubsystems = new Reflections(config);
+            packageReflections.merge(reflectionsWithSubsystems);
+        }
 
         // We need the class predicate to include classes in subsystems and whatnot. We can't change it in an
         // existing module, so make a new one based on the one from the moduleFactory.
-        Module engine = new Module(
+        return new Module(
                 packageModule.getMetadata(),
                 packageModule.getResources(),
-                Collections.emptyList(),
+                classPaths,
                 packageReflections,
-                packageModule.getClassPredicate().or(clazz ->
-                        reflectionsWithSubsystems.getConfiguration()
-                                .getUrls().contains(ClasspathHelper.forClass(clazz)))
+                clazz ->
+                        packageModule.getClassPredicate().test(clazz)
+                                || config.getUrls().contains(ClasspathHelper.forClass(clazz))
         );
+    }
 
-        registry.add(engine);
-        return engine;
+    static boolean isInAnnotatedPackage(Class<?> clazz) {
+        Package pkg = clazz.getPackage();
+        if (pkg.isAnnotationPresent(API.class)) {
+            return true;
+        }
+        String name = pkg.getName();
+        int pos = name.lastIndexOf(".");
+        while (pos > 0) {
+            Package ancestor = Package.getPackage(name.substring(0, pos));
+            if (ancestor == null) {
+                return false;
+            } else if (ancestor.isAnnotationPresent(API.class)) {
+                return true;
+            }
+            pos = name.lastIndexOf(".", pos - 1);
+        }
+        return false;
     }
 
     public ModuleManager(Config config) {
@@ -162,7 +213,7 @@ public class ModuleManager {
         this(config.getNetwork().getMasterServer(), classesOnClasspathsToAddToEngine);
     }
 
-    private ModuleMetadataJsonAdapter newMetadataReader() {
+    static ModuleMetadataJsonAdapter newMetadataReader() {
         final ModuleMetadataJsonAdapter metadataJsonAdapter = new ModuleMetadataJsonAdapter();
         for (ModuleExtension ext : StandardModuleExtension.values()) {
             metadataJsonAdapter.registerExtension(ext.getKey(), ext.getValueType());
@@ -256,8 +307,10 @@ public class ModuleManager {
         return newEnvironment;
     }
 
+    @Deprecated(/*since="4.4.0"*/)
     public ModuleMetadataJsonAdapter getModuleMetadataReader() {
-        // FIXME: is this used anywhere? (MTE?)
+        // FIXME: This has one use in MTE. Give MTE a better interface to use and remove
+        //     the accessor for this.
         return metadataReader;
     }
 
@@ -266,6 +319,23 @@ public class ModuleManager {
     }
 
     public PermissionProvider getPermissionProvider(Module module) {
-        return permissionProviderFactory.createPermissionProviderFor(module, module.getClassPredicate());
+        return permissionProviderFactory.createPermissionProviderFor(module, x -> false);
     }
+
+    static File urlToFile(URL url) {
+        URL fileUrl = url;
+        if (url.getProtocol().equals("jar")) {
+            try {
+                JarURLConnection connection = (JarURLConnection) url.openConnection();
+                fileUrl = connection.getJarFileURL();
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to get file from " + url);
+            }
+        }
+        try {
+            return new File(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Failed to get file from " + url, e);
+        }
+    };
 }
